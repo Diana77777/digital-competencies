3feadf324d964cdef902c172b3c1f1c4179f9778
Klimovich <diana_klimovich@bk.ru>
2022-10-19 17:15:09 +0300

    galp start

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass")(require("sass"));
+
+// Static server
+gulp.task("server", function () {
+  browserSync.init({
+    server: {
+      baseDir: "src ",
+    },
+  });
+});
+
+gulp.task("styles", function () {
+  return gulp
+    .src("src/sass/*.+(scss | sass)")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("watch", function () {
+  gulp.watch("src/sass/*.+(scss | sass)", gulp.parallel("styles"));
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.parallel("watch", "server", "styles"));
+
+gulp.task("brow", function () {
+  browserSync.init({
+    server: {
+      baseDir: "src",
+    },
+    browser: "google chrome",
+    notify: false,
+    open: "local",
+  });
+});
